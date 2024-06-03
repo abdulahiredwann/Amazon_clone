a46341dcec064dd7c61f849308cb204f4a0fb92f
Abdulahi <123581480+abdulahiredwann@users.noreply.github.com>
2024-06-03 15:14:48 +0300

    css

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css'
 import { BrowserRouter } from 'react-router-dom';
 import { StateProvider } from './Components/StateProvider/StateProvider';
 import reducer, { initialState } from './Components/Reducer/Reducer';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
